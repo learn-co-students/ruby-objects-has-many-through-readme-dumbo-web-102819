be632da236f319364fa454696c9be4741a580450
GAierken <g.aierken@gmail.com>
2019-11-04 14:19:57 -0500

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,38 @@
+require "pry"
+
 class Customer
+    attr_accessor :name, :age
+    @@all = []
+def initialize (name, age)
+    @name = name
+    @age = age
   
-end
+    @@all << self
+end ## initialize
+
+def self.all
+    @@all
+end ### all
+
+
+def new_meal(waiter, total, tip)
+   Meal.new(waiter, self, total, tip)
+
+end ### new meal
+
+def meals
+    Meal.all.select do |meal|
+    meal.customer == self
+    end ## select
+
+end ### meals
+
+def waiters
+    meals.map do |meal|
+    meal.waiter
+    end ## map
+    
+end ### waiters
+
+
+end ## customer class
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,48 @@
+require "pry"
 class Waiter
 
-end
+    attr_accessor :name, :years_of_experience
+
+@@all = []
+
+def initialize (name, years_of_experience)
+    @name = name 
+    @years_of_experience = years_of_experience
+
+    @@all << self
+end ## initialize
+
+def self.all
+    @@all
+end  ### all
+
+
+def new_meal (customer, total, tip)
+    Meal.new(self, customer, total, tip)
+
+end ## new meal
+
+def meals
+    Meal.all.select do |meal|
+    meal.waiter == self
+    end ##select
+end ###meals
+
+
+
+
+
+def best_tipper
+   
+    #a.max { |a, b| a.length <=> b.length }
+    best_tipper_meal = meals.max do |meal_a,meal_b|
+     
+    meal_a.tip <=> meal_b.tip
+    end ##max
+    best_tipper_meal.customer
+   
+end ## best tipper
+
+
+
+end ### class waiter
